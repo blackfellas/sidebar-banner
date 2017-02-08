e940bbdc9c49b1a4868077ac03826ebbc2722980
blackfellas <c727621@trbvn.com>
2017-02-08 17:18:36 -0500

    Python 3

--- login.py
+++ login.py
@@ -2,40 +2,40 @@
 # -*- coding: utf-8 -*-
 
 import praw
-import logging, logging.config, re, sys, os
-from ConfigParser import SafeConfigParser
+import logging, logging.config, os
+from configparser import ConfigParser
+global r
 
-r = None
-cfg_file = SafeConfigParser()
-path_to_cfg = os.path.abspath(os.path.dirname(sys.argv[0]))
-path_to_cfg = os.path.join(path_to_cfg, 'schedulebot.cfg')
+cfg_file = ConfigParser()
+path_to_cfg = os.getcwd() #os.path.abspath(os.path.dirname(sys.argv[0]))
+path_to_cfg = os.path.join(path_to_cfg, 'cred.cfg')
 cfg_file.read(path_to_cfg)
 
 def login():
     try:
-        r = praw.Reddit(user_agent=cfg_file.get('reddit', 'user_agent'))
-        r.set_oauth_app_info(cfg_file.get('reddit', 'client_id'), cfg_file.get('reddit', 'client_secret'), cfg_file.get('reddit', 'redirect_uri'))
-        r.refresh_access_information(cfg_file.get('reddit', 'refresh_token')) 
+        r = praw.Reddit(user_agent=cfg_file.get('reddit', 'user_agent'), client_id = cfg_file.get('reddit', 'client_id'), client_secret = cfg_file.get('reddit', 'client_secret'), username = cfg_file.get('reddit', 'username'), password = cfg_file.get('reddit', 'password'))
         
     except Exception as e:
-        logging.error(e)
-        print ('trying old methods...')
-        try:
-            logging.debug('Logging in as {0}'
-                      .format(cfg_file.get('reddit', 'username')))
-            r.login(cfg_file.get('reddit', 'username'),
-                cfg_file.get('reddit', 'password'), disable_warning=False)
-        except Exception as e:
-            logging.error(e)
-            
-    finally:
-        return r
+         logging.error(e)           
+    return r
     
-    
+#def conn():
+#    dbname = cfg_file.get('database', 'database')
+#    dbuser = cfg_file.get('database', 'user')
+#    dbpassword = cfg_file.get('database', 'password')
+#    dbhost = cfg_file.get('database', 'host')
+#    
+#    try:    
+#        con = psycopg2.connect(database=dbname, user=dbuser, password=dbpassword, host=dbhost)
+#        print('  connected to database: ' + dbname)
+#        return con
+#    except Exception as e:
+#        logging.error(e)
+#    return
 
 if __name__ == '__main__':
-    print 'login.py'
+    print ('login.py')
     login()
 else:
-    print 'login module imported'
+    print ('  login module imported')
     
